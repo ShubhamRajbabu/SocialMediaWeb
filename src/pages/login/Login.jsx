f1de69cc89ba200eabd2db8f1e951064411efa04
ShubhamRajbabu <shubhamshroff0810@gmail.com>
2024-05-26 12:41:41 +0530

    login and register page UI

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import './login.scss';
+import React from "react";
+import "./login.scss";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi ut
+            labore possimus sed expedita harum natus perspiciatis, quae incidunt
+            dignissimos!
+          </p>
+          <span>Don't you have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import './register.scss'
+import React from "react";
+import "./register.scss";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="register">
+      <div className="card">
+        
+        <div className="left">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Register</button>
+          </form>
+        </div>
+        <div className="right">
+          <h1>Shubbu Social.</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi ut
+            labore possimus sed expedita harum natus perspiciatis, quae incidunt
+            dignissimos!
+          </p>
+          <span>Already have an account?</span>
+          <button>Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
