213d0ffdf1abc69baba41bf7334293826fccb38b
ShubhamRajbabu <shubhamshroff0810@gmail.com>
2024-05-27 18:38:53 +0530

    updated stories data

--- src/pages/stories/Stories.jsx
+++ src/pages/stories/Stories.jsx
@@ -5,26 +5,73 @@ import { AuthContext } from "../../context/AppContext";
 const Stories = () => {
   const { currUser } = useContext(AuthContext);
 
+  // const stories = [
+  //   {
+  //     id: 1,
+  //     name: "John Doe",
+  //     img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "John Doe",
+  //     img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "John Doe",
+  //     img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+  //   },
+  //   {
+  //     id: 4,
+  //     name: "John Doe",
+  //     img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+  //   },
+  // ];
   const stories = [
     {
       id: 1,
-      name: "John Doe",
-      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+      name: "Shubham",
+      userId: 1,
+      profilePic:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
     },
     {
       id: 2,
-      name: "John Doe",
-      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+      name: "Shubham",
+      userId: 2,
+      profilePic:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "Tenetur iste voluptates dolorem rem commodi voluptate pariatur, voluptatum, laboriosam consequatur enim nostrum cumque! Maiores a nam non adipisci minima modi tempore.",
+      img: "https://images.pexels.com/photos/15578747/pexels-photo-15578747/free-photo-of-sneaking-cat-on-the-meadow.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     },
     {
       id: 3,
-      name: "John Doe",
-      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+      name: "Shubham",
+      userId: 1,
+      profilePic:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      img: "https://images.pexels.com/photos/24035320/pexels-photo-24035320/free-photo-of-feet-of-a-girl-wearing-white-shoes-sitting-by-a-bunch-of-freshly-picked-tulips.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     },
     {
       id: 4,
-      name: "John Doe",
-      img: "https://images.pexels.com/photos/13916254/pexels-photo-13916254.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+      name: "Shubham",
+      userId: 1,
+      profilePic:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      img: "https://images.pexels.com/photos/24244074/pexels-photo-24244074/free-photo-of-processed-with-vsco-with-av8-preset.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 5,
+      name: "Shubham",
+      userId: 1,
+      profilePic:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      img: "https://images.pexels.com/photos/23459284/pexels-photo-23459284/free-photo-of-fisch.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     },
   ];
 
